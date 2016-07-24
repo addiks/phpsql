f3726d01da4b6df6ff8bdb4b4a3e0b189d40cbe0
Gerrit Addiks <gerrit@addiks.de>
2016-07-24 23:36:03 +0200

    fix for show sql parser

--- src/Addiks/PHPSQL/Job/Statement/ShowStatement.php
+++ src/Addiks/PHPSQL/Job/Statement/ShowStatement.php
@@ -14,6 +14,7 @@ namespace Addiks\PHPSQL\Job\Statement;
 use Addiks\PHPSQL\Value\Enum\Sql\Show\ShowType;
 use Addiks\PHPSQL\Job\StatementJob;
 use Addiks\PHPSQL\Executor\ShowExecutor;
+use Addiks\PHPSQL\Job\Part\ValuePart;
 
 /**
  *
@@ -24,30 +25,63 @@ class ShowStatement extends StatementJob
     const EXECUTOR_CLASS = ShowExecutor::class;
 
     private $type;
-    
+
     public function setType(ShowType $type)
     {
         $this->type = $type;
     }
-    
+
     public function getType()
     {
         return $this->type;
     }
-    
+
     private $database;
-    
+
     public function setDatabase($database)
     {
         $this->database = (string)$database;
     }
-    
+
     public function getDatabase()
     {
         return $this->database;
     }
-    
+
     public function getResultSpecifier()
     {
     }
+
+    protected $isFull = false;
+
+    public function setIsFull($isFull)
+    {
+        $this->isFull = (bool)$isFull;
+    }
+
+    public function isFull()
+    {
+        return $this->isFull;
+    }
+
+    /**
+     * @var ValuePart
+     */
+    protected $conditionValue;
+
+    public function setConditionValue(ValuePart $conditionValue)
+    {
+        if (is_null($conditionValue)) {
+            $this->conditionValue = null;
+
+        } else {
+            $this->conditionValue = $conditionValue;
+        }
+    }
+
+    public function getConditionValue()
+    {
+        return $this->conditionValue;
+    }
+
 }
--- src/Addiks/PHPSQL/SqlParser/ShowSqlParser.php
+++ src/Addiks/PHPSQL/SqlParser/ShowSqlParser.php
@@ -11,60 +11,95 @@
 
 namespace Addiks\PHPSQL\SqlParser;
 
+use ErrorException;
 use Addiks\PHPSQL\Value\Enum\Sql\Show\ShowType;
 use Addiks\PHPSQL\Job\Statement\ShowStatement;
 use Addiks\PHPSQL\Value\Enum\Sql\SqlToken;
 use Addiks\PHPSQL\Iterators\SQLTokenIterator;
 use Addiks\PHPSQL\SqlParser\SqlParser;
-
 use Addiks\PHPSQL\Iterators\TokenIterator;
+use Addiks\PHPSQL\Exception\MalformedSqlException;
+use Addiks\PHPSQL\SqlParser\Part\ValueParser;
+use Addiks\PHPSQL\Job\Part\ValuePart;
 
 class ShowSqlParser extends SqlParser
 {
-    
+
+    /**
+     * @var ValueParser
+     */
+    protected $valueParser;
+
+    public function setValueParser(ValueParser $valueParser)
+    {
+        $this->valueParser = $valueParser;
+    }
+
+    public function getValueParser()
+    {
+        return $this->valueParser;
+    }
+
     public function canParseTokens(SQLTokenIterator $tokens)
     {
         return is_int($tokens->isTokenNum(SqlToken::T_SHOW(), TokenIterator::CURRENT))
             || is_int($tokens->isTokenNum(SqlToken::T_SHOW(), TokenIterator::NEXT));
     }
-    
+
     public function convertSqlToJob(SQLTokenIterator $tokens)
     {
-        
+        /* @var $valueParser ValueParser */
+        $valueParser = $this->valueParser;
+
         $tokens->seekTokenNum(SqlToken::T_SHOW());
-        
+
         if ($tokens->getCurrentTokenNumber() !== SqlToken::T_SHOW()) {
             throw new ErrorException("Tried to convert sql-show to job-entity when tokeniterator does not point to T_SHOW!");
         }
-        
+
         $showJob = new ShowStatement();
-        
+
+        if ($tokens->seekTokenNum(SqlToken::T_FULL())) {
+            $showJob->setIsFull(true);
+        }
+
         switch(true){
 
             case $tokens->seekTokenNum(SqlToken::T_DATABASES()):
                 $showJob->setType(ShowType::DATABASES());
                 break;
-                
+
             case $tokens->seekTokenNum(SqlToken::T_TABLES()):
                 $showJob->setType(ShowType::TABLES());
                 break;
-                
+
             case $tokens->seekTokenNum(SqlToken::T_VIEWS()):
                 $showJob->setType(ShowType::VIEWS());
                 break;
-            
+
             default:
                 throw new MalformedSqlException("Invalid parameter for show-statement!", $tokens);
         }
-        
+
         if ($tokens->seekTokenNum(SqlToken::T_FROM())) {
             if (!$tokens->seekTokenNum(T_STRING)) {
                 throw new MalformedSqlException("Missing database name after FROM in SHOW statement!");
             }
-            
+
             $showJob->setDatabase($tokens->getCurrentTokenString());
         }
-        
+
+        if ($tokens->seekTokenNum(SqlToken::T_WHERE())) {
+            if (!$valueParser->canParseTokens($tokens)) {
+                throw new MalformedSqlException("Missing valid condition-value after WHERE in SHOW statement!");
+            }
+
+            /* @var $conditionValue ValuePart */
+            $conditionValue = $valueParser->convertSqlToJob($tokens);
+
+            $showJob->setConditionValue($conditionValue);
+        }
+
         return $showJob;
     }
 }
